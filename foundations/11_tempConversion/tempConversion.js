257d5b79f520e6fb0d663b6ec80d650f33ab4139
Karl De Mesa <karlenzo.dm@gmail.com>
2026-07-30 03:16:20 +0800

    Solution: foundations/11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
 const convertToCelsius = function() {
+  return (fahrenheit - 32) * (5/9)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return (celsius * (5/9)) + 32
 };
 
 // Do not edit below this line
